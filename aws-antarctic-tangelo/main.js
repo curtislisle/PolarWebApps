5e6b6bb815d85c601067d0706225413737cde98b
Curtis Lisle <clisle@knowledgevis.com>
2014-11-16 09:38:28 -0500

    coordinate transform successfully added for AWS station positions by using Proj4js to convert to ESPG:3031 before adding points to OpenLayers

--- aws-antarctic-tangelo/main.js
+++ aws-antarctic-tangelo/main.js
@@ -1,27 +1,38 @@
 
 
+function addStaticVectorLayer() {
 
+    // now that the data is back and loaded, setup the new map layer This has to be done inside this
+    // call so the data is ready when it fires off.  Without this call inside the function, the layer could be 
+    // added to the map before the data load is finished
+
+    
+	var interesting = new OpenLayers.Layer.Vector("Interesting", {
+		projection: 'EPSG:4326',
+		strategies: [new OpenLayers.Strategy.Fixed()],
+			protocol: new OpenLayers.Protocol.HTTP({
+			url: "interesting.json",
+			format: new OpenLayers.Format.GeoJSON()
+		})
+	});
+	antarctic.map.addLayer(interesting);
+	antarctic.interesting = interesting;
+}
 
 
 function addVectorLayer(layer) {
 
+	// creating source and destination Proj4js objects
+	// once initialized, these may be re-used as often as needed
+	var source = new Proj4js.Proj('EPSG:4326');    //source coordinates will be in Longitude/Latitude
+	var dest = new Proj4js.Proj('EPSG:3031');     //destination coordinates in meters from the south pole
+
+
+
     d3.json("service/getdata/"+layer, function (error, jsonreturned) {
         console.log(jsonreturned,"\n");
 
-        // now that the data is back and loaded, setup the new map layer This has to be done inside this
-        // call so the data is ready when it fires off.  Without this call inside the function, the layer could be 
-        // added to the map before the data load is finished
-
-        /*
-		var markers = new OpenLayers.Layer.Vector("Markers", {
-			projection: 'EPSG:4326',
-			strategies: [new OpenLayers.Strategy.Fixed()],
-				protocol: new OpenLayers.Protocol.HTTP({
-				url: "interesting.json",
-				format: new OpenLayers.Format.GeoJSON()
-			})
-		});
-*/
+ 
 
         // we want opaque external graphics and non-opaque internal graphics
         var layer_style = OpenLayers.Util.extend({}, OpenLayers.Feature.Vector.style['default']);
@@ -43,9 +54,13 @@ function addVectorLayer(layer) {
 		var featureList = []
 		var markers = new OpenLayers.Layer.Vector("Markers",{projection: 'EPSG:3031'});
 		for (var i = jsonreturned['data'].length - 1; i >= 0; i--) {
-			console.log(jsonreturned['data'][i]['lng'], jsonreturned['data'][i]['lat'])
-			var lng_float = jsonreturned['data'][i]['lng']
-			var lat_float = jsonreturned['data'][i]['lat']
+
+			// transforming point coordinates from longitude,latitude to a projection in meters centered at the South Pole
+			var stationpoint = new Proj4js.Point(jsonreturned['data'][i]['lng'],jsonreturned['data'][i]['lat']);   //any object will do as long as it has 'x' and 'y' properties
+			Proj4js.transform(source, dest, stationpoint);      //do the transformation.  x and y are modified in place
+			//console.log(jsonreturned['data'][i]['lng'], jsonreturned['data'][i]['lat'],'xformed:',stationpoint.x,stationpoint.y)
+			var lng_float = stationpoint.x
+			var lat_float = stationpoint.y
 			//var point = new OpenLayers.Geometry.Point(jsonreturned['data'][i]['lng'], jsonreturned['data'][i]['lat']);
 			var point = new OpenLayers.Geometry.Point(lng_float, lat_float);
 
@@ -56,22 +71,7 @@ function addVectorLayer(layer) {
 		antarctic.map.addLayer(markers);
 		//console.log(featureList)
 
-		/*
-		var markers = new OpenLayers.Layer.Vector( "Markers" );
-		antarctic.map.addLayer(markers);
-
-		var size = new OpenLayers.Size(21,25);
-		var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-		var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
-
-		for (var i = jsonreturned.length - 1; i >= 0; i--) {
-			if (i == 0) {
-				markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(jsonreturned['data'][i]['lng'], jsonreturned['data'][i]['lat']),icon));
-			} else {
-				markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(jsonreturned['data'][i]['lng'], jsonreturned['data'][i]['lat']),icon.clone()));
-			}
-		};
-		*/
+		
 
 		antarctic.markers = markers
 		map = antarctic.map
@@ -99,16 +99,19 @@ function addVectorLayer(layer) {
 }
 
 
+antarctic = {}
+Proj4js.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+Proj4js.defs["EPSG:3031"] = "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+Proj4js.defs["EPSG:900913"] = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs";
+
+
 // this function is called as soon as the page is finished loading
 window.onload = function () {   
 
-	antarctic = {}
+
 	antarctic.map = null
 	antarctic.markers = null
-
-	Proj4js.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
-	Proj4js.defs["EPSG:3031"] = "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
-	Proj4js.defs["EPSG:900913"] = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs";
+	antarctic.interesting = null
 
 	var map = new OpenLayers.Map('map', {
 		controls: [
@@ -143,5 +146,7 @@ window.onload = function () {
 	)
 
 	map.addLayer(tiles);
-	addVectorLayer('all_stations');
+	addStaticVectorLayer();
+	addVectorLayer('stations');
+
 }
